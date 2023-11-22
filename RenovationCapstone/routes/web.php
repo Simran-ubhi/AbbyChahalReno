7698120ed76835e80f11508f7cddd6c909ce848a
simran-ubhi <simranubhi68@gmail.com>
2023-11-22 16:21:46 -0500

    cli
    cli

--- RenovationCapstone/routes/web.php
+++ RenovationCapstone/routes/web.php
@@ -7,6 +7,56 @@ use App\Http\Controllers\UserController;
 
 
 
+
+// Storage link
+
+Route::get('/storagelink', function(){
+    Artisan::call('storage:link');
+});
+
+Route::get('/storageunlink', function(){
+    Artisan::call('storage:unlink');
+});
+
+
+//Clear Cache=
+Route::get('/clear-cache', function() {
+    $exitCode = Artisan::call('cache:clear');
+    return '<h1>Cache facade value cleared</h1>';
+});
+
+//Reoptimized
+Route::get('/optimize', function() {
+    $exitCode = Artisan::call('optimize');
+    return '<h1>Reoptimized class loader</h1>';
+});
+
+//Route cache:
+Route::get('/route-cache', function() {
+    $exitCode = Artisan::call('route:cache');
+    return '<h1>Routes cached</h1>';
+});
+
+//Clear Route cache:
+Route::get('/route-clear', function() {
+    $exitCode = Artisan::call('route:clear');
+    return '<h1>Route cache cleared</h1>';
+});
+
+//Clear View cache:
+Route::get('/view-clear', function() {
+    $exitCode = Artisan::call('view:clear');
+    return '<h1>View cache cleared</h1>';
+});
+
+//Clear Config cache:
+Route::get('/config-cache', function() {
+    $exitCode = Artisan::call('config:cache');
+    return '<h1>Clear Config cleared</h1>';
+});
+
+
+
 // User Controller
 
 Route::get('/',[UserController::class,'welcome'])->name('index');
